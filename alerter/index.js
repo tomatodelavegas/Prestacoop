266f971d25f9a8c6e35a8ac57624422db34f739b
Geoffrey Bossut <geoffrey.bossut@epita.fr>
2020-06-06 18:03:23 +0200

    [index.js]: Now with specific id mongodb retrieval

--- alerter/index.js
+++ alerter/index.js
@@ -1,11 +1,8 @@
-//const http = require("http");
 const socketio = require("socket.io");
 const express = require("express");
 const assert = require("assert");
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 let app = express();
-
-
 
 // server setup with express JS
 
@@ -40,25 +37,6 @@ let alerts = [
         Registration_State: "NY"
     }
 ];
-
-let alertsid = [0, 1];
-
-app.get("/", function (req, res) {
-    res.send(alertsid); // TODO: send db ids with time
-});
-
-app.get("/:id", function (req, res) {
-    let id = req.params.id; // TODO: string -> int conversion checking
-    if (id >= alerts.length || id < 0) {
-        console.error(`Invalid id ${id}`);
-        res.status(500).send(`Invalid id ${id}`);
-    }
-    res.send(alerts[id]);
-});
-
-var server = app.listen(port, hostname, function () {
-    console.log(`Server running http://${hostname}:${port}`);
-});
 
 /**
 let server = http.createServer((req, res) => {
@@ -108,6 +86,7 @@ let mongourl = `mongodb://${mongousername}:${mongopwd}@${mongohostname}:${mongop
 
 console.log(mongourl);
 
+/**
 MongoClient.connect(mongourl, function (err, client) {
     assert.equal(null, err);
     console.log("Connected successfully to mongodb");
@@ -117,5 +96,58 @@ MongoClient.connect(mongourl, function (err, client) {
 
     alertsColection.insertOne(alerts[0]);
 
+    //db.close();
+
     client.close();
+});
+**/
+
+
+/**
+ ** Get all alerts
+ **/
+app.get("/", function (req, res) {
+    let resArr = [];
+    MongoClient.connect(mongourl, function (err, client) {
+        assert.equal(null, err);
+        const db = client.db(mongodbname);
+        const alertsColection = db.collection("alerts");
+        let cursor = alertsColection.find();
+        cursor.forEach((doc, err) => {
+            assert.equal(null, err);
+            resArr.push(doc);
+        }, () => {
+            client.close();
+            res.send(resArr);
+        });
+    });
+});
+
+/**
+ ** Getting a specific alert by id
+ **/
+app.get("/:id", async function (req, res) {
+    let id = req.params.id; // TODO: string -> int conversion checking
+    try {
+        let o_id = new ObjectId(id);
+        const client = await MongoClient.connect(mongourl).catch(err => res.status(500).send(err));
+        if (client == null)
+            return res.status(500).send("Failed to connect to database");
+        const db = client.db(mongodbname);
+        const alertsColection = db.collection("alerts");
+        const elm = await alertsColection.findOne({ _id: o_id }).catch(err => res.status(500).send(err));
+        if (typeof client !== "undefined" && client)
+            client.close();
+        if (elm == null)
+            return res.status(500).send(`No such element _id: ${id}`);
+        res.send(elm);
+    } catch (err) {
+        if (typeof client !== "undefined" && client)
+            client.close();
+        res.status(500).send(`No such element _id: ${id}`);
+    }
+});
+
+var server = app.listen(port, hostname, function () {
+    console.log(`Server running http://${hostname}:${port}`);
 });
