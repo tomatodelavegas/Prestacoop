f0acb0b7075e9b723ccbd4bef968915f90e0e0f0
Sabrina Meng <sabrina.meng@epita.fr>
2020-06-08 00:02:44 +0200

    [Drone_ID] Removed triple call to random

--- src/main/scala/Prestacoop/Drone_simulation.scala
+++ src/main/scala/Prestacoop/Drone_simulation.scala
@@ -18,13 +18,14 @@ object Drone_simulation {
     val producer: KafkaProducer[String, String] = getDefaultKafkaProducer
 
     val rand = scala.util.Random
+    val rand_ID = rand.nextInt(20000)
 
     val data = file.getLines().drop(1)
     data.foreach{ line =>
-      if (getDroneMsg(rand.nextInt(20000), line, columnsId).Violation_Code == -1) //send into alert stream
-        producer.send(new ProducerRecord[String, String]("alert", getDroneMsg(rand.nextInt(20000), line, columnsId).asJson.toString))
+      if (getDroneMsg(rand_ID, line, columnsId).Violation_Code == -1) //send into alert stream
+        producer.send(new ProducerRecord[String, String]("alert", getDroneMsg(rand_ID, line, columnsId).asJson.toString))
       else
-        producer.send(new ProducerRecord[String, String]("general", getDroneMsg(rand.nextInt(20000), line, columnsId).asJson.toString))
+        producer.send(new ProducerRecord[String, String]("general", getDroneMsg(rand_ID, line, columnsId).asJson.toString))
       Thread.sleep((rand.nextFloat()*100).toInt)
     }
 
